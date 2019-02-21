4c4623c68470b769cc3ffc94c6ffda5e429526fc
Thales Burakowski <thales.burakowski@fcamara.com.br>
2019-02-21 18:08:34 -0300

    login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,55 @@
 <template>
-  <div class="fields">
-    <div class="line-inputs">
-      <div>
-        <label class="label-input">
-          <input type="text" required>
-          <div class="label-text">Usuario</div>
-        </label>
-      </div>
-      <div>
+  <div class="page">
+    <h1 class="page-title">Login</h1>
+    <div class="fields">
+      <div class="line-inputs">
         <div>
           <label class="label-input">
-            <input type="text" maxlength="80" required>
-            <div class="label-text">Senha</div>
+            <input type="text" required>
+            <div class="label-text">Usuario</div>
           </label>
         </div>
+        <div>
+          <div>
+            <label class="label-input">
+              <input type="password" maxlength="80" required>
+              <div class="label-text">Senha</div>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="save">
+        <button class="button button-principal">Login</button>
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-.line-inputs {
-  grid-template-columns: 25% 25% 25%;
+.page {
+  .page-title {
+    margin-left: 21vw;
+  }
+  .fields {
+    margin-left: 20vw;
+    width: 50%;
+    .line-inputs {
+      grid-template-columns: 100%;
+      grid-gap: 2em;
+    }
+    .save {
+      justify-content: flex-end;
+      margin-right: 2rem;
+    }
+  }
 }
 </style>
 
-
 <script>
 export default {
-  name: "Login",
+  name: 'Login',
   data() {
-    return{
-      
-    }
-  }
-};
+    return {}
+  },
+}
 </script>
