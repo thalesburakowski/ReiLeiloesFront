03b8aeb098e2639c7f6c44f6452fd67de3f5b3f7
unknown <melissa@espressolabs.com.br>
2019-02-22 00:25:51 -0300

    History.vue: copiado da antiga home

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,17 +1,95 @@
 <template>
-  <div></div>
+  <div class="page">
+    <div class="title-new">
+      <h1 class="page-title">Seus Leilões</h1>
+    </div>
+    <!-- <div class="page-advanced-search">
+      <div class="page-advanced-search-label">Pesquisa Avançada</div>
+      <div class="page-advanced-search-select">
+        <Multiselect
+          v-model="value"
+          :options="options"
+          :multiple="true"
+          :close-on-select="false"
+          placeholder="Pesquise por categorias"
+          selectLabel="Pressione Enter para selecionar"
+          deselectLabel="Pressione Enter para remover"
+        ></Multiselect>
+      </div>
+    </div>-->
+    <div class="table">
+      <div class="table-line">
+        <div class="table-line-title" v-for="title in titles" :key="title">{{ title }}</div>
+      </div>
+      <div v-for="auction in auctions" :key="auction">
+        <div class="table-line">
+          <div class="item">{{ auction.name }}</div>
+          <div class="item">{{ auction.category }}</div>
+          <div class="item">{{ auction.starts }}</div>
+          <div class="item">{{ auction.ends }}</div>
+          <div class="item">{{ auction.value }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Multiselect from 'vue-multiselect'
 export default {
-  name: "History",
+  name: 'History',
+  components: { Multiselect },
   data() {
-    return{
-      
+    return {
+      value: null,
+      options: [
+        'Obras de arte',
+        'Colecionaveis',
+        'Brinquedos',
+        'Automotivo',
+        'Outros',
+      ],
+      titles: ['Nome', 'Categoria', 'Início', 'Tempo Restante', 'Valor Atual'],
+      auctions: [
+        {
+          name: 'Teste Leilão',
+          category: 'Outros',
+          starts: '20/20/2019',
+          ends: '17 horas',
+          value: 233.5,
+        },
+      ],
     }
-  }
-};
+  },
+}
 </script>
-<style lang="sass" scoped>
-
+<style lang="scss">
+// .page-advanced-search {
+//   display: flex;
+//   justify-content: flex-end;
+//   margin: 1rem;
+//   .page-advanced-search-label {
+//     display: flex;
+//     justify-content: center;
+//     align-self: center;
+//     font-family: 'Dosis-Medium';
+//     margin-right: 2rem;
+//   }
+//   .page-advanced-search-select {
+//     width: 25vw;
+//   }
+// }
+.page {
+  & .page-title {
+    margin-left: 0;
+  }
+}
+.table-line {
+  grid-template-columns: 25% 25% 20% 20% 10%;
+  .item:nth-child(4) {
+    padding-right: 2rem;
+  }
+}
 </style>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
